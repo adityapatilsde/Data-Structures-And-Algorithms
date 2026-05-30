08b56b7e130e07c72245da250d9d2323b442c47f
aditya <adityapatilsde@gmail.com>
2026-05-30 19:12:04 +0530

    started with DSA foundation course in Java , today types of loops in java

--- BasicsofJava/NestedIfElse/NoOfDaysInAMonth.java
+++ BasicsofJava/NestedIfElse/NoOfDaysInAMonth.java
@@ -5,19 +5,50 @@ public class NoOfDaysInAMonth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of days in the month: ");
-        int days = sc.nextInt();
+        System.out.print("Enter the month number: ");
+        int monthNo = sc.nextInt();
 
-        if(days == 28 ) {
-            System.out.println("February");
-        } else if (days == 31) {
-            System.out.println("April , June , September , November");
+        switch (monthNo){
+            case 1:
+                System.out.println("January 31 days");
+                break;
+            case 2:
+                System.out.println("February 28 days");
+                break;
+            case 3:
+                System.out.println("March 31 days");
+                break;
+            case 4:
+                System.out.println("April 30 days");
+                break;
+            case 5:
+                System.out.println("May 31 days");
+                break;
+            case 6:
+                System.out.println("June 30 days");
+                break;
+            case 7:
+                System.out.println("July 31 days");
+                break;
+            case 8:
+                System.out.println("August 31 days");
+                break;
+            case 9:
+                System.out.println("September 30 days");
+                break;
+            case 10:
+                System.out.println("October 31 days");
+                break;
+            case 11:
+                System.out.println("November 30 days");
+                break;
+            case 12:
+                System.out.println("December 31 days");
+                break;
+            default:
+                System.out.println("Invalid month input");
         }
-        else if(days == 30) {
-            System.out.println("January , February , March , May , July , August , October , December");
-        }
-        else {
-            System.out.println("Invalid days input");
-        }
+
+
     }
 }
