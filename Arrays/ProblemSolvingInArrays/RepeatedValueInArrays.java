f4245d6286b118705350a9a39f22de2a7133bfa2
aditya <adityapatilsde@gmail.com>
2026-06-02 15:50:44 +0530

    Arrays problem solving using method functions and loops

---
+++ Arrays/ProblemSolvingInArrays/RepeatedValueInArrays.java
@@ -0,0 +1,39 @@
+package Arrays.ProblemSolvingInArrays;
+
+import Arrays.LoopsInArrays.ProblemSolving.SumOfArrElements;
+
+import java.util.Scanner;
+
+public class RepeatedValueInArrays {
+    static int repeatedElements(int [] arr , int x){
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == x){
+             count++;
+            }
+        }
+        return count;
+    }
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the value of X: ");
+        int x = sc.nextInt();
+
+        System.out.println("COUNT OF X: "+ repeatedElements(arr, x));
+
+
+    }
+}
