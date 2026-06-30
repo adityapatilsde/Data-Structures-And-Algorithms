b20887c9ed6ef265db1639a4cc5dbfe7189cf463
aditya <adityapatilsde@gmail.com>
2026-06-30 16:12:52 +0530

    String Basic functions

---
+++ StringsBasics/Performance.java
@@ -0,0 +1,11 @@
+package StringsBasics;
+
+public class Performance {
+    public static void main(String[] args) {
+        String str = "";
+        for (int i = 0; i <= 10; i++){
+            str = str + i;
+        }
+        System.out.println(str);
+    }
+}
