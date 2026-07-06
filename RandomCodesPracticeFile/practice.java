62be9216a395fdc9ad823e5fb9ee7880d37d6782
aditya <adityapatilsde@gmail.com>
2026-07-06 22:16:59 +0530

    sorting algorithms practice and revision

--- RandomCodesPracticeFile/practice.java
+++ RandomCodesPracticeFile/practice.java
@@ -2,66 +2,83 @@ package RandomCodesPracticeFile;
 
 import java.util.Scanner;
 public class practice {
-    static void printArray(int arr[]){
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+static void printArray(int arr[]){
+    for (int i = 0; i < arr.length; i++){
+        System.out.print(arr[i]+" ");
     }
-    static void swap(int arr[], int i,int j){
+    System.out.println();
+}
+static void bubbleSort(int arr[]){
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++ ){
+        boolean flag = false;
+        for (int j = 0; j < n - i - 1; j++){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            flag = true;
+        }
+        if (flag == false){
+            return;
+        }
+    }
+    printArray(arr);
+}
+static void selectionSort(int arr[]){
+    for (int i = 0; i < arr.length - 1; i++){
+        int min_index = i;
+        for (int j = i + 1; j < arr.length; j++){
+            if (arr[j] < arr[min_index]){
+                min_index = j;
+            }
+        }
         int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
     }
-    static int partition(int arr[],int start,int end){
-        int pivot = arr[start]; // selecting first element as pivot index
-        int count = 0; // setting count as 0 well use this to count no of elements smaller than pivot element
-        for (int i = 1; i <= end; i++){  // this loop counts total no of elements smaller than pivot element
-            if (arr[i] < pivot){
-                count++;
-            }
+}
+static void merge(int arr[], int l, int mid, int r){
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+    int left[] = new int[n1];
+    int right[] = new int[n2];
+    int i ,j ,k ;
+    for (i = 0; i < n1; i++) left[i] = arr[l+i];
+    for (j = 0; j < n2; j++) right[j] = arr[mid+1+j];
+    i = 0; j = 0; k = l;
+
+    while (i < n1 && j < n2){
+        if (left[i] < right[j]){
+            arr[k++] = left[i++];
         }
-        int pivot_index = start+count; //calculating pivot element index
-        swap(arr,start,pivot_index); // setting pivot element to its correct position
-
-        int i = start, j = end; // using 2 pointers start and end of the array
-        while (i < pivot_index && j > pivot_index){
-            while (arr[i] <= pivot) i++; // here left elements are checked w.r.t to pivot and loop stops at misplaced element position
-            while (arr[j] > pivot) j++; // here right elements are checked w.r.t to pivot and loop stops at misplaced element position
-
-            if (i < pivot_index && j > pivot_index){ // here the misplaced elements index are checked and swapped to its correct position
-                swap(arr,i,j); // misplaced elements are swapped
-                i++;
-                j--;
-            }
+        else {
+            arr[k++] = right[j++];
         }
-        return pivot_index;
     }
-    static void quickSort(int arr[],int st,int end){
-        if (st >= end) return;
-        int pi = partition(arr,st,end);
-        quickSort(arr,st,pi-1);
-        quickSort(arr,pi+1,end);
+    while (i < n1){
+        arr[k++] = left[i++];
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the size of the array: ");
-        int n = sc.nextInt();
-        int arr[]= new int[n];
-
-        System.out.println("Enter "+n+" elements: ");
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.println("Original Array: ");
-        printArray(arr);
-
-        System.out.println("Sorted array by quickSort: ");
-        quickSort(arr,0,arr.length-1);
-        printArray(arr);
-
+    while (j < n2){
+        arr[k++] = right[j++];
     }
+}
+static void mergesort(int arr[],int left, int right){
+    if (left >= right) return;
+    int mid = (left + right) / 2;
+    mergesort(arr,left,mid);
+    mergesort(arr,mid+1,right);
+    merge(arr,left,mid,right);
+}
+  public static void main(String[] args) {
+      int arr[] = {50,40,30,20,10};
+      System.out.println("Orignal Array: ");
+      printArray(arr);
+     // bubbleSort(arr);
+      System.out.println("Sorted Array: ");
+//      selectionSort(arr);
+//      bubbleSort(arr);
+      mergesort(arr,0,arr.length-1);
+      printArray(arr);
+  }
  }
 
