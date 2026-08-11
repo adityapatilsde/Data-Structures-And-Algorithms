58b4eb2e6a964b67174c007a9431e3cf5ecda9fb
aditya <adityapatilsde@gmail.com>
2026-08-11 23:29:07 +0530

    revision

--- ArraysRevision/arraysRevise.java
+++ ArraysRevision/arraysRevise.java
@@ -259,7 +259,7 @@ public class arraysRevise {
         for (int i = 0; i < arr.length - 1; i++){
             boolean flag = false;
             for (int j = 0; j < arr.length - 1 - i ; j++){
-                if (arr[i] > arr[j]){
+                if (arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
