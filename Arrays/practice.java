c6be784115e2f11d2136ee05e1f52a9630ee086c
aditya <adityapatilsde@gmail.com>
2026-09-24 23:26:33 +0530

    Arrays basic revision

--- Arrays/practice.java
+++ Arrays/practice.java
@@ -1,38 +1,36 @@
 package Arrays;
 
-import java.util.Random;
-import java.util.Scanner;
+interface smartDevice{
+    void  turnon();
+    void  turnOff();
+}
+class smartPhone implements smartDevice{
+    @Override
+    public void turnon(){
+        System.out.println("SmartPhone is turned on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Smart phone is turned off");
+    }
+}
+class smartTV implements smartDevice{
+    @Override
+    public  void turnon(){
+        System.out.println("Tv is on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Tv is off");
+    }
+}
 public class practice {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String arr[] = {"Rock","Paper","Scissors"};
 
-        System.out.println("Enter 0 - rock, enter 1 - paper, enter 2 - Sccissors");
-        System.out.println();
-        System.out.print("Enter user choice: ");
-        int userChoice = sc.nextInt();
-        System.out.println();
-        Random random = new Random();
-        int computerChoice = random.nextInt(3);
-        System.out.println("User choice "+arr[userChoice]);
-        System.out.println("Computer choice "+arr[computerChoice]);
+        smartDevice d;
+        d = new smartTV();
+        d.turnOff();
+        d.turnon();
 
-        System.out.println();
-
-            System.out.println("RESULT");
-            if (userChoice == computerChoice) {
-                System.out.println("Its a drawl");
-            }
-        else if (userChoice == 0 && computerChoice == 2) {
-                System.out.println("User wins");
-            } else if (userChoice == 1 && computerChoice == 0) {
-                System.out.println("User wins");
-            } else if (userChoice == 2 && computerChoice == 1) {
-                System.out.println("User wins");
-            } else {
-                System.out.println("Computer wins");
-            }
     }
-   }
-
-
+}
